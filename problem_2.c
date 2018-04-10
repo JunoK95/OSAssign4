7cba88aa91cfae82aa1bb6661962b3aa2bca9654
junok <junok@lab1-7.eng.utah.edu>
2018-04-09 19:25:49 -0600

    finished 6, needs sleep on 1 and 2

---
+++ problem_2.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <assert.h>
+
+signed int threadCount;
+volatile int Number[99];
+volatile int entering[99];
+
+volatile int csCount[99];
+volatile int in_cs = 0;
+
+void lock(int thread) {
+    entering[thread] = 1;
+    __sync_synchronize();
+    int max_ticket = 0;
+    int i = 0;
+    for (i = 0; i < threadCount; ++i) {
+        int ticket = Number[i];
+        max_ticket = ticket > max_ticket ? ticket : max_ticket;
+        sched_yield();
+    }
+    Number[thread] = max_ticket + 1;
+    __sync_synchronize();
+    entering[thread] = 0;
+    __sync_synchronize();
+    int j = 0;
+    for (j = 0; j < threadCount; ++j) {
+        while (entering[j]) { sched_yield();}
+        __sync_synchronize();
+        while (Number[j] != 0 &&
+               (Number[j] < Number[thread] ||
+                (Number[j] == Number[thread] && j < thread))) { sched_yield();}
+    }
+}
+
+void unlock(int thread) {
+    __sync_synchronize();
+    Number[thread] = 0;
+}
+
+void critical_section(int thread) {
+    //Checks to see only one is in criticalsection
+    assert(in_cs==0);
+    in_cs++;
+    assert(in_cs==1);
+    in_cs++;
+    assert(in_cs==2);
+    in_cs++;
+    assert(in_cs==3);
+    in_cs=0;
+    // printf("%d using resource...\n", thread);
+    csCount[thread]++;
+    printf("in cs %d times\n", csCount[thread]);
+    __sync_synchronize();
+}
+
+void *thread_body(void *arg) {
+  while(1){
+    long thread = (long)arg;
+    lock(thread);
+    critical_section(thread);
+    unlock(thread);
+  }
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3){
+      printf("%s\n", "Enter Number of Thread and Number of Seconds Only");
+      exit(-1);
+    }
+    threadCount = atoi(argv[1]);
+    long long int secondLimit = atoll(argv[2]);
+    printf("%lli\n", secondLimit );
+
+    pthread_t threads[threadCount];
+    int i = 0;
+
+    for (i = 0; i < threadCount; ++i) {
+        pthread_create(&threads[i], NULL, &thread_body, (void*)((long)i));
+    }
+
+    for (i = 0; i < threadCount; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
+    for (i = 0; i < threadCount; ++i) {
+      printf("in cs %d times\n", csCount[i]);
+    }
+
+    return 0;
+}
