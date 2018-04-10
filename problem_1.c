126461bcb37fb99ad2ac62107b46f698d7d4f8ab
junok <junok@lab1-7.eng.utah.edu>
2018-04-09 19:14:59 -0600

    problem 1 complete but needs to sleep()

--- problem_1.c
+++ problem_1.c
@@ -7,47 +7,41 @@
 #include <assert.h>
 
 signed int threadCount;
-volatile int tickets[99];
-volatile int choosing[99];
+volatile int Number[99];
+volatile int entering[99];
 
 volatile int csCount[99];
-volatile int resource;
 volatile int in_cs = 0;
 
 void lock(int thread) {
-    choosing[thread] = 1;
+    entering[thread] = 1;
     __sync_synchronize();
     int max_ticket = 0;
     int i = 0;
     for (i = 0; i < threadCount; ++i) {
-        int ticket = tickets[i];
+        int ticket = Number[i];
         max_ticket = ticket > max_ticket ? ticket : max_ticket;
     }
-    tickets[thread] = max_ticket + 1;
+    Number[thread] = max_ticket + 1;
     __sync_synchronize();
-    choosing[thread] = 0;
+    entering[thread] = 0;
     __sync_synchronize();
-    int other = 0;
-    for (other = 0; other < threadCount; ++other) {
-        while (choosing[other]) { }
+    int j = 0;
+    for (j = 0; j < threadCount; ++j) {
+        while (entering[j]) { }
         __sync_synchronize();
-        while (tickets[other] != 0 &&
-               (tickets[other] < tickets[thread] ||
-                (tickets[other] == tickets[thread] && other < thread))) { }
+        while (Number[j] != 0 &&
+               (Number[j] < Number[thread] ||
+                (Number[j] == Number[thread] && j < thread))) { }
     }
 }
 
 void unlock(int thread) {
     __sync_synchronize();
-    tickets[thread] = 0;
+    Number[thread] = 0;
 }
 
 void critical_section(int thread) {
-    if (resource != 0) {
-        printf("Resource was acquired by %d, but is still in-use by %d!\n",
-               thread, resource);
-    }
-    resource = thread;
     //Checks to see only one is in criticalsection
     assert(in_cs==0);
     in_cs++;
@@ -57,20 +51,19 @@ void critical_section(int thread) {
     in_cs++;
     assert(in_cs==3);
     in_cs=0;
-    printf("%d using resource...\n", thread);
+    // printf("%d using resource...\n", thread);
     csCount[thread]++;
     printf("in cs %d times\n", csCount[thread]);
     __sync_synchronize();
-    sleep(2);
-    resource = 0;
 }
 
 void *thread_body(void *arg) {
+  while(1){
     long thread = (long)arg;
     lock(thread);
     critical_section(thread);
     unlock(thread);
-    return NULL;
+  }
 }
 
 int main(int argc, char **argv) {
@@ -78,15 +71,9 @@ int main(int argc, char **argv) {
       printf("%s\n", "Enter Number of Thread and Number of Seconds Only");
       exit(-1);
     }
-
     threadCount = atoi(argv[1]);
     long long int secondLimit = atoll(argv[2]);
     printf("%lli\n", secondLimit );
-
-    memset((void*)tickets, 0, sizeof(tickets));
-    memset((void*)choosing, 0, sizeof(choosing));
-    memset((void*)csCount, 0, sizeof(choosing));
-    resource = 0;
 
     pthread_t threads[threadCount];
     int i = 0;
@@ -97,6 +84,10 @@ int main(int argc, char **argv) {
 
     for (i = 0; i < threadCount; ++i) {
         pthread_join(threads[i], NULL);
+    }
+
+    for (i = 0; i < threadCount; ++i) {
+      printf("in cs %d times\n", csCount[i]);
     }
 
     return 0;
